fe25b96cfd527b1bf3914229200ff66d9b26ace6
Daniel Tralamazza <tralamazza@gmail.com>
2011-07-24 02:42:22 -0300

    bulk example

--- examples/query.js
+++ examples/query.js
@@ -4,8 +4,25 @@ var RapleafApi = require('../index');
 var key = process.argv[2];
 var email = process.argv[3];
 
-var rap = new RapleafApi(key);
+var client = new RapleafApi(key);
 
-rap.query_by_email(email, function(data) {
-  console.log(data);
+client.query_by_email(email, function(data, err) {
+  console.log('direct query test');
+  if (err !== undefined)
+    console.log('rapleaf error: ' + err);
+  else
+    console.log(data);
+}).on('error', function(err) {
+    console.log('request error: ' + err);
 });
+
+client.bulk_by_email([email, email], function(data, err) {
+  console.log('bulk test');
+  if (err !== undefined)
+    console.log('rapleaf error: ' + err);
+  else
+    console.log(data);
+}).on('error', function(err) {
+    console.log('request error: ' + err);
+});
+
